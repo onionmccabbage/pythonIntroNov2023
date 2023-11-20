6f3c502868d167800d2107d0fca6c64e6de2b14d
onionmccabbage <onionmccabbage@live.com>
2023-11-20 10:39:22 +0000

    python basics

---
+++ basics.py
@@ -0,0 +1,16 @@
+# Python is a scripting language. It is interpreted by the python executable
+
+# identifiers, variables and immediate code
+
+a = 3 # an int
+b = 7 # a floating point value
+print(a/b, type(a/b))
+# maths: + - / * // ** and %
+print(3**2) # ** is to the power of
+print(b//a) # how many times does it go into...
+print(b%a) # modulo arythmetic
+# we have += and -= also *= /= but NOT ++ or --
+
+# Python has an immutable string type
+s = 'is it coffee yet?'
+print(s)
