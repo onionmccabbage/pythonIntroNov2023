46c48146e80ebda5eff982ec89b1fd329f91b24e
onionmccabbage <onionmccabbage@live.com>
2023-11-21 10:55:59 +0000

    using system argument variables

---
+++ using_sysargv.py
@@ -0,0 +1,4 @@
+import sys
+# Every argument provided to python will exist in a list of strings called sys.argv
+# there is always at least ONE system argument variable - the filename
+print(sys.argv) # these are any arguments passed in to python
