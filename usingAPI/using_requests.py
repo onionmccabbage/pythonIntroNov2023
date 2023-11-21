6f5f83d861ce8ce3e72867b80d299375050d6e89
onionmccabbage <onionmccabbage@live.com>
2023-11-21 12:25:07 +0000

    using requets for JSON data

--- usingAPI/using_requests.py
+++ usingAPI/using_requests.py
@@ -1,5 +1,5 @@
 # API is application programming interface
-# it means an eternal resourcce we can access
+# it means an eternal resource we can access
 # use usually use the 'requests' library to acccess an API
 # we used to use urllib (or urllib3)
 # we must make sure the requests library is available to python
@@ -7,14 +7,22 @@
 
 import requests
 
-def makeRequest():
+def makeRequest(whichUser):
     '''call an API'''
-    url = 'https://jsonplaceholder.typicode.com/users/1'
-    response = requests.get(url)
-    return response.json() # convert the JSON data into a Python structure
+    # wevcan use any available end-point
+    url = f'https://jsonplaceholder.typicode.com/users/{whichUser}'
+    # it is a really good idea to surround requests with try block
+    try:
+        response = requests.get(url) # or post, update, put ...
+        # we might retrieve text, xml, html json ...
+        # NB this URL will only return JSON
+        return response.json() # convert the JSON data into a Python structure
+    except Exception as e:
+        return f'Something went wrong {e}'
 
 if __name__ == '__main__':
-    data = makeRequest()
+    id = input('Which user ID? ') # id can be 1-9
+    data = makeRequest(id)
     print(data)
 
 
