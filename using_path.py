bb668ca79b3fc5a41e6dc85578802daca943f9b2
onionmccabbage <onionmccabbage@live.com>
2023-11-21 13:59:25 +0000

    details about sys.path and import

--- using_path.py
+++ using_path.py
@@ -1,4 +1,12 @@
 # we can always see where python will look for any import
 import sys
+# it is a reelly good idea to share a location for useful in-house libraries
+# then add that path to sys,path
+
+# we can add places that Python should look for imports
+sys.path.append('D:\\pythonIntroNov2023\\useful\\subpack') # or use /
+import more #  this will resolve
 
 print( sys.path )
+
+print(more.here()) # this is found on our path
