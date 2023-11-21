f510868c2754bf70501172033a02344fd0aa4c17
onionmccabbage <onionmccabbage@live.com>
2023-11-21 16:25:07 +0000

    using doctest

--- using_doctest.py
+++ using_doctest.py
@@ -1,3 +1,16 @@
+# we can unit test a module to ensure riguour
+import doctest
+
+def cube(x):
+    '''return the cube of x
+    >>> cube(3)
+    27
+    >>> cube(-2)
+    -8'''
+    return x**3
 
 
-def 
+if __name__ == '__main__':
+    # c = cube(3)
+    # print(c) # 27
+    doctest.testmod(verbose=True)
