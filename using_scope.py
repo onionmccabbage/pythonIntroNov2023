bd1d15a064ea162c3bc4ef04f5198b79c02c39f5
onionmccabbage <onionmccabbage@live.com>
2023-11-21 11:30:41 +0000

    using global scope

--- using_scope.py
+++ using_scope.py
@@ -3,10 +3,16 @@ count = 1 # this is in the global scope
 
 def fn(): # this is a block scope
     global count # we now have access to the global variable
-    count = 9
+    count = 9 # this is indented, so it is inside a block. It is in a block scope
+    return count
+
+def other():
+    count = 3 # a different block scope
     return count
 
 if __name__ == '__main__':
     print(count) # 1
     print(fn()) # 9
+    print(other()) # 3
+    # print(count) # 1
     print(count) # 9
