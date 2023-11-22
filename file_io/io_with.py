5e6bb84560cc438c4fb9386415f1aa2bd0c35a9e
onionmccabbage <onionmccabbage@live.com>
2023-11-22 15:56:13 +0000

    using 'with' to cleanly close an asset

---
+++ file_io/io_with.py
@@ -0,0 +1,14 @@
+# we can use the 'with' operator to ensure clean working with file access objects
+
+def writeCleanly(t):
+    '''Here we write text to a file and automatically 
+    close the file access object'''
+    try:
+        with open('my_log.txt', 'a') as fobj:
+            fobj.write(t) # I could use chunks to write
+        # When the 'with' operator ends, the file access object will be automatically closed
+    except Exception as err:
+        print(f'Oh dear... {err}')
+        
+if __name__ == '__main__':
+    writeCleanly('this is nicely output and the file access object is automatically closed')
